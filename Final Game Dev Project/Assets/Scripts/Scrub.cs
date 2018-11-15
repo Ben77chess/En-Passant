8667fefc6ee65d363c602d4dd1991b85443ebb97
gadeuneo <8356171+gadeuneo@users.noreply.github.com>
2018-11-15 10:33:14 +0100

    SLIDER

---
+++ Final Game Dev Project/Assets/Scripts/Scrub.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Scrub : MonoBehaviour {
+
+
+    public Slider bar;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(bar.value);
+	}
+}
