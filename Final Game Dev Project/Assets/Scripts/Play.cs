d0d13d8ed2b4ab6a1bd50fe52e6132f2afcb7367
gadeuneo <8356171+gadeuneo@users.noreply.github.com>
2018-11-15 11:15:22 +0100

    UI Changes

---
+++ Final Game Dev Project/Assets/Scripts/Play.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Play : MonoBehaviour {
+
+    public Toggle play;
+    public Toggle pause;
+    public Slider scrub;
+
+
+    // Use this for initialization
+    void Start () {
+
+        play.isOn = false;
+        pause.isOn = false;
+        scrub.value = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (pause.isOn) {
+            play.isOn = false;
+        }
+
+        else if (play.isOn)
+        {
+            pause.isOn = false;
+            scrub.value = scrub.value * Time.deltaTime;
+        }
+
+        else
+        {
+            play.isOn = false;
+            pause.isOn = false;
+        }
+
+	}
+}
