da2825747af47391f74a07279114880edc198487
gadeuneo <8356171+gadeuneo@users.noreply.github.com>
2018-12-01 15:30:16 +0100

    Dialoge box camera

--- Final Game Dev Project/Assets/UIFaceCamera.cs
+++ Final Game Dev Project/Assets/UIFaceCamera.cs
@@ -4,14 +4,43 @@ using UnityEngine;
 
 public class UIFaceCamera : MonoBehaviour {
 
+    private CameraControl camCtrl;
+    private Camera[] camList;
+    private Camera currCam;
+
     Transform cameraTransform;
 	// Use this for initialization
 	void Start () {
+        /*
+        foreach (Camera cam in camList)
+        {
+            if(cam.enabled == true)
+            {
+                currCam = cam;
+            }
+        }
+
+        cameraTransform = currCam.transform;
+        */
         cameraTransform = Camera.main.transform;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
+
+        /*
+        foreach (Camera cam in camList)
+        {
+            if(cam.enabled == true)
+            {
+                currCam = cam;
+            }
+        }
+
+        cameraTransform = currCam.transform;
+        */
+
+
         Vector3 temp = transform.position - cameraTransform.position;
         temp.y = 0;
         transform.rotation = Quaternion.LookRotation(temp, Vector3.up);
