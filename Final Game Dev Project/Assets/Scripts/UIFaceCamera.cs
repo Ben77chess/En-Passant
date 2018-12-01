fa8dd6205aade12523bc7ce216ca712a13443e15
gadeuneo <8356171+gadeuneo@users.noreply.github.com>
2018-12-01 16:34:48 +0100

    Dialouge fix

--- Final Game Dev Project/Assets/Scripts/UIFaceCamera.cs
+++ Final Game Dev Project/Assets/Scripts/UIFaceCamera.cs
@@ -11,24 +11,17 @@ public class UIFaceCamera : MonoBehaviour {
     Transform cameraTransform;
 	// Use this for initialization
 	void Start () {
-        /*
-        foreach (Camera cam in camList)
-        {
-            if(cam.enabled == true)
-            {
-                currCam = cam;
-            }
-        }
-
-        cameraTransform = currCam.transform;
-        */
-        cameraTransform = Camera.main.transform;
+        setUp();
+        //cameraTransform = Camera.main.transform;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
 
-        /*
+        
+        camCtrl = GetComponent<CameraControl>();
+
+        camList = camCtrl.cameras;
         foreach (Camera cam in camList)
         {
             if(cam.enabled == true)
@@ -36,13 +29,30 @@ public class UIFaceCamera : MonoBehaviour {
                 currCam = cam;
             }
         }
-
+        
         cameraTransform = currCam.transform;
-        */
+        
 
 
         Vector3 temp = transform.position - cameraTransform.position;
         temp.y = 0;
         transform.rotation = Quaternion.LookRotation(temp, Vector3.up);
 	}
+
+    private void setUp()
+    {
+        camCtrl = GetComponent<CameraControl>();
+
+        camList = camCtrl.cameras;
+
+        foreach (Camera cam in camList)
+        {
+            if (cam.enabled == true)
+            {
+                currCam = cam;
+            }
+        }
+
+        cameraTransform = currCam.transform;
+    }
 }
